f14bcaa1b40b340964c912c044f70cb6087320c7
Hyacinthe Compaoré <131278191+compaoreh338@users.noreply.github.com>
2025-10-03 07:47:35 +0000

    FIX: remove user setting module

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -1,20 +1,19 @@
 <?php
 
 return [
-    'user' => [
-        'NewsletterRecipients' => [
-            'parent' => 'user',
-            'position' => ['after' => 'user_setup'],
-            'access' => 'user,group',
-            'iconIdentifier' => 'extensions-newsletter-recipients-recipients',
-            'labels' => 'LLL:EXT:newsletter_recipients/Resources/Private/Language/tx_newsletter_recipient.xlf',
-            'extensionName' => 'NewsletterRecipients',
-            'controllerActions' => [
-                \Fab\NewsletterRecipients\Controller\NewsletterRecipientController::class => [
-                    'editMany',
-                    'updateMany',
-                ],
-            ],
-        ],
-    ],
+//    'user' => [
+//        'NewsletterRecipients' => [
+//            'parent' => 'user',
+//            'position' => ['after' => 'user_setup'],
+//            'access' => 'user,group',
+//            'iconIdentifier' => 'EXT:newsletter_recipients/ext_icon.svg',
+//            'extensionName' => 'NewsletterRecipients',
+//            'controllerActions' => [
+//                \Fab\NewsletterRecipients\Controller\NewsletterRecipientController::class => [
+//                    'editMany',
+//                    'updateMany',
+//                ],
+//            ],
+//        ],
+//    ],
 ];
